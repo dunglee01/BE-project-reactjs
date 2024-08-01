ab52817d7576614fdf9ad467d3ccd24ac85be28f
dunglv <vietdung.le@vmgmedia.vn>
2024-08-01 15:41:01 +0700

    update

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import {connectDb} from './db.js'
 import { AuthRouter, ProductRouter } from './routers/index.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './swagger.js';
-import fs from 'fs';
 import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 
 const app = express();
 // Middleware for parsing request body
@@ -23,13 +24,16 @@ app.use(cors());
 //     allowedHeaders: ['Content-Type'], 
 //   })
 // );
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const options = { customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.css' };
 
-// const spec = JSON.parse(
-//     fs.readFileSync(path.join(__dirname, '../petstore-api.json'), 'utf8')
-// );
+const spec = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8')
+);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, options));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(spec, options));
 app.use('/api/v1', AuthRouter);
 app.use('/api/v1', ProductRouter);
 
