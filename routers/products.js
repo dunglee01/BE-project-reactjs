6665d478c2ee8efb8e2952cff59798cefac1c23e
dunglv <vietdung.le@vmgmedia.vn>
2024-08-01 17:04:35 +0700

    update get product

--- routers/products.js
+++ routers/products.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createProduct } from "../controller/product.js";
+import { createProduct, getProduct } from "../controller/product.js";
 
 const router = express.Router();
 
-
 router.post('/product', createProduct)
+router.get('/product', getProduct)
 
 export default router
