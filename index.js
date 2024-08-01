c8b157fb9140e0e92245e955cc3ad298bc345e2e
dunglv <vietdung.le@vmgmedia.vn>
2024-08-01 15:57:35 +0700

    update code

--- index.js
+++ index.js
@@ -3,27 +3,15 @@ import cors from 'cors';
 import {connectDb} from './db.js'
 import { AuthRouter, ProductRouter } from './routers/index.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swagger.js';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import helmet from 'helmet';
 
 const app = express();
-// Middleware for parsing request body
 app.use(express.json());
-// app.use(bodyParser.json());
-
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
 app.use(cors());
-// Option 2: Allow Custom Origins
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type'], 
-//   })
-// );
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -33,23 +21,16 @@ const spec = JSON.parse(
     fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8')
 );
 
+const cspDefaults = helmet.contentSecurityPolicy.getDefaultDirectives();
+delete cspDefaults['upgrade-insecure-requests'];
+
+app.use(helmet({
+    contentSecurityPolicy: { directives: cspDefaults }
+}));
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(spec, options));
 app.use('/api/v1', AuthRouter);
 app.use('/api/v1', ProductRouter);
-
-// app.use('/books', booksRoute);
-
-// mongoose
-//   .connect(mongoDBURL)
-//   .then(() => {
-//     console.log('App connected to database');
-//     app.listen(PORT, () => {
-//       console.log(`App is listening to port: ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
 
 const start = () => {
     connectDb(app);
